3c0d708ee3d41ccacb3e752b0cc9c58ef1298e69
Terence Lim <terence.lim@salesforce.com>
2021-12-10 14:54:07 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { Client } = require('pg');
+
+const app = express()
+const port = 3000
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/user', (req, res) => {
+  client.query('SELECT * FROM salesforce.eureka_customers__c;', (err, result) => {
+    if (err) throw err;
+    for (let row of result.rows) {
+      console.log(JSON.stringify(row));
+    }
+    res.send(result.rows);
+    client.end();
+  });  
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
