34e545cfe784ecc984bbf452e72b1acb8419290a
Terence Lim <terence.lim@salesforce.com>
2022-01-02 15:15:45 +0800

    feat: full implementation of post field update

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+var bodyParser = require("body-parser");
+
 dotenv.config();
 
 const { Client } = require("pg");
 
 const app = express();
 
+app.use(express.json()); // Used to parse JSON bodies
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 // either set port from env provided by Heroku, or load 3000 for localhost
 const PORT = process.env.PORT || 3000;
 
@@ -54,10 +62,10 @@ app.get("/customers/:userId", (req, res) => {
 /* post to update customers */
 
 app.post("/customers", (req, res) => {
-  const { id, name } = req.headers;
+  const { id, name, email, address, phone } = req.body;
   client.query(
-    "UPDATE salesforce.eureka_customers__c SET name__c = $1 WHERE id = $2;",
-    [name, id],
+    "UPDATE salesforce.eureka_customers__c SET name__c = $2, email__c = $3, address__c = $4, phone__c = $5 WHERE id = $1;",
+    [id, name, email, address, phone],
     (err, result) => {
       if (err) throw err;
       for (let row of result.rows) {
